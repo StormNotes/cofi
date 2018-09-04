68f07262f7588e39db7b8a931ffb4efb271d34ff
Leonardo Diaz <stormnotesnotes@gmail.com>
2018-09-04 18:40:43 +0200

    update

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+function myMenu() {
+  document.getElementById("cofi").style.display = "block";
+  document.getElementById("coffee-order").style.display = "block";
+}
+
+
+function addAmerican() {
+  cafAme = 0;
+  cafAme + 1;
+  cafAme++;
+  console.log(cafAme);
+}
+
+function addDouble() {
+  cafDou = 0;
+  cafDou + 1;
+  cafDou++;
+  console.log(cafDou);
+}
+
+function addExpresso() {
+  cafExpesso = 0;
+  cafExpesso + 1;
+  cafExpesso++;
+  console.log(cafExpesso);
+}
+
+function addLatte() {
+  cafLatte = 0;
+  cafLatte + 1;
+  cafLatte++;
+  console.log(cafLatte);
+}
